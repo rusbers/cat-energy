7cebb121b0eac72b4f209da9cc32b3836309b4af
Ruslan B <74817877+rusbers@users.noreply.github.com>
2021-05-04 20:42:10 +0300

    Автоматизируй сборку
    В gulpfile.js были добавлены следующие задачи:
    * минификация стилей;
    * оптимизация изображений;
    * копирование нужных файлов в папку build.

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,14 @@ const sass = require("gulp-sass");
 const postcss = require("gulp-postcss");
 const postcssUrl = require("postcss-url");
 const autoprefixer = require("autoprefixer");
+const csso = require("postcss-csso");
+const htmlmin = require("gulp-htmlmin");
+const terser = require("gulp-terser");
+const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp");
 const svgsprite = require("gulp-svg-sprite");
 const rename = require("gulp-rename");
+const del = require("del");
 const sync = require("browser-sync").create();
 
 // Styles
@@ -20,14 +26,55 @@ const styles = () => {
       postcssUrl({
         assetsPath: "../"
       }),
-      autoprefixer()
+      autoprefixer(),
+      csso()
     ]))
+    .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(sync.stream());
 }
 
 exports.styles = styles;
+
+// HTML
+
+const html = () => {
+  return gulp.src("source/*.html")
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest("build"));
+}
+
+// Images
+
+const imageOptimization = () => {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.mozjpeg({progressive: true}),
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("build/img"))
+}
+
+exports.imageOptimization = imageOptimization;
+
+const copyImages = () => {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+   .pipe(gulp.dest("build/img"))
+}
+
+exports.copyImages = copyImages;
+
+// webP
+
+const createWebp = () => {
+  return gulp.src("source/img/**/*.{png,jpg}")
+    .pipe(webp({quality: 80}))
+    .pipe(gulp.dest("build/img"))
+}
+
+exports.createWebp = createWebp;
 
 // Svg stack
 
@@ -40,17 +87,41 @@ const svgstack = () => {
       }
     }))
     .pipe(rename("stack.svg"))
-    .pipe(gulp.dest("source/img"));
+    .pipe(gulp.dest("build/img"));
 }
 
 exports.svgstack = svgstack;
+
+// Copy
+
+const copy = (done) => {
+  gulp.src([
+    "source/fonts/*.{woff2,woff}",
+    "source/*.ico",
+    "source/*.webmanifest",
+  ],{
+    base: "source"
+  })
+    .pipe(gulp.dest("build"))
+  done();
+}
+
+exports.copy = copy;
+
+// Clean
+
+const clean = () => {
+  return del("build");
+}
+
+exports.clean = clean;
 
 // Server
 
 const server = (done) => {
   sync.init({
     server: {
-      baseDir: 'source'
+      baseDir: "build"
     },
     cors: true,
     notify: false,
@@ -61,14 +132,51 @@ const server = (done) => {
 
 exports.server = server;
 
+// Reload
+
+const reload = (done) => {
+  sync.reload();
+  done();
+}
+
 // Watcher
 
 const watcher = () => {
   gulp.watch("source/sass/**/*.scss", gulp.series("styles"));
   gulp.watch("source/img/icons/**/*.svg", gulp.series("svgstack"));
-  gulp.watch("source/*.html").on("change", sync.reload);
+  gulp.watch("source/*.html", gulp.series(html, reload));
 }
 
+// Build
+
+const build = gulp.series(
+  clean,
+  copy,
+  imageOptimization,
+  gulp.parallel(
+    styles,
+    html,
+    svgstack,
+    createWebp
+  ),
+);
+
+exports.build = build;
+
+// Default
+
 exports.default = gulp.series(
-  styles, svgstack, server, watcher
+  clean,
+  copy,
+  copyImages,
+  gulp.parallel(
+    styles,
+    html,
+    svgstack,
+    createWebp
+  ),
+  gulp.series(
+    server,
+    watcher
+  )
 );
